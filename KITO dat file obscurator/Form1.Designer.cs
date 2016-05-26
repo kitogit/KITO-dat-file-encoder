97d0687332c99b6be857a38a8c42ea94aaa9df71
kito <kito@kito.com>
2016-05-26 15:08:27 +0200

    Version 8

---
+++ KITO dat file obscurator/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace IT_XOR7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputFileButton = new System.Windows.Forms.Button();
+            this.InputFileTextBox = new System.Windows.Forms.TextBox();
+            this.OutputFileTextBox = new System.Windows.Forms.TextBox();
+            this.OutputFileButton = new System.Windows.Forms.Button();
+            this.DoTheMap = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.uLineBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tLineBox = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InputFileButton
+            // 
+            this.InputFileButton.Location = new System.Drawing.Point(576, 10);
+            this.InputFileButton.Name = "InputFileButton";
+            this.InputFileButton.Size = new System.Drawing.Size(75, 23);
+            this.InputFileButton.TabIndex = 0;
+            this.InputFileButton.Text = "Input file";
+            this.InputFileButton.UseVisualStyleBackColor = true;
+            this.InputFileButton.Click += new System.EventHandler(this.InputFileButton_Click);
+            // 
+            // InputFileTextBox
+            // 
+            this.InputFileTextBox.Location = new System.Drawing.Point(13, 12);
+            this.InputFileTextBox.Name = "InputFileTextBox";
+            this.InputFileTextBox.ReadOnly = true;
+            this.InputFileTextBox.Size = new System.Drawing.Size(557, 20);
+            this.InputFileTextBox.TabIndex = 1;
+            // 
+            // OutputFileTextBox
+            // 
+            this.OutputFileTextBox.Location = new System.Drawing.Point(13, 39);
+            this.OutputFileTextBox.Name = "OutputFileTextBox";
+            this.OutputFileTextBox.ReadOnly = true;
+            this.OutputFileTextBox.Size = new System.Drawing.Size(454, 20);
+            this.OutputFileTextBox.TabIndex = 2;
+            // 
+            // OutputFileButton
+            // 
+            this.OutputFileButton.Location = new System.Drawing.Point(575, 39);
+            this.OutputFileButton.Name = "OutputFileButton";
+            this.OutputFileButton.Size = new System.Drawing.Size(75, 23);
+            this.OutputFileButton.TabIndex = 3;
+            this.OutputFileButton.Text = "Output file";
+            this.OutputFileButton.UseVisualStyleBackColor = true;
+            this.OutputFileButton.Click += new System.EventHandler(this.OutputFileButton_Click);
+            // 
+            // DoTheMap
+            // 
+            this.DoTheMap.Location = new System.Drawing.Point(576, 69);
+            this.DoTheMap.Name = "DoTheMap";
+            this.DoTheMap.Size = new System.Drawing.Size(75, 23);
+            this.DoTheMap.TabIndex = 4;
+            this.DoTheMap.Text = "DO IT";
+            this.DoTheMap.UseVisualStyleBackColor = true;
+            this.DoTheMap.Click += new System.EventHandler(this.DoTheMap_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "dat files|*.dat|Text files|*.txt";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "dat files|*.dat|Text files|*.txt";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Placer text";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(473, 39);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(93, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = ".dat file output";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Swap input and output";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(70, 85);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(100, 20);
+            this.keyBox.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "XOR Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(257, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "default IT key";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(335, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "7*(i%7+1)";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(176, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Save key";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Controls.Add(this.uLineBox);
+            this.flowLayoutPanel1.Controls.Add(this.label4);
+            this.flowLayoutPanel1.Controls.Add(this.tLineBox);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(335, 63);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(230, 26);
+            this.flowLayoutPanel1.TabIndex = 13;
+            // 
+            // uLineBox
+            // 
+            this.uLineBox.Location = new System.Drawing.Point(3, 3);
+            this.uLineBox.Name = "uLineBox";
+            this.uLineBox.ReadOnly = true;
+            this.uLineBox.Size = new System.Drawing.Size(100, 20);
+            this.uLineBox.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(109, 3);
+            this.label4.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 18);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "/";
+            // 
+            // tLineBox
+            // 
+            this.tLineBox.Location = new System.Drawing.Point(127, 3);
+            this.tLineBox.Name = "tLineBox";
+            this.tLineBox.ReadOnly = true;
+            this.tLineBox.Size = new System.Drawing.Size(100, 20);
+            this.tLineBox.TabIndex = 2;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(495, 98);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(156, 17);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "preChecksum (default: true)";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 122);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DoTheMap);
+            this.Controls.Add(this.OutputFileButton);
+            this.Controls.Add(this.OutputFileTextBox);
+            this.Controls.Add(this.InputFileTextBox);
+            this.Controls.Add(this.InputFileButton);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(679, 160);
+            this.MinimumSize = new System.Drawing.Size(679, 160);
+            this.Name = "Form1";
+            this.Text = "KITO XOR Encode/Decode revision 8";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button InputFileButton;
+        private System.Windows.Forms.TextBox InputFileTextBox;
+        private System.Windows.Forms.TextBox OutputFileTextBox;
+        private System.Windows.Forms.Button OutputFileButton;
+        private System.Windows.Forms.Button DoTheMap;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox uLineBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tLineBox;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
